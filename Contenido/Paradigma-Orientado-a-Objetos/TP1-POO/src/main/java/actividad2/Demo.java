83a58a1e75dfc7f49dbaba4a227d28a5cd600853
Lucas Montaño <lucasmo24@gmail.com>
2024-08-26 21:00:03 -0300

    Cambios en Demo actividad2

--- Contenido/Paradigma-Orientado-a-Objetos/TP1-POO/src/main/java/actividad2/Demo.java
+++ Contenido/Paradigma-Orientado-a-Objetos/TP1-POO/src/main/java/actividad2/Demo.java
@@ -10,23 +10,45 @@ public class Demo {
         CuentaBancaria cuenta2 = new CuentaSueldo();
         System.out.println(cuenta2);
 
-        CuentaSueldo cuenta3 = new CuentaSueldo(546546);
-        System.out.println(cuenta3);
+        System.out.println("\n----- Se intenta crear una cuenta con el CBU 546546");
 
-        CuentaSueldo cuenta4 = new CuentaSueldo(987654321987654321L);
+        //CuentaSueldo cuenta3 = new CuentaSueldo(546546);
+        //System.out.println(cuenta3);
+
+        System.out.println("\n----- Se intenta crear una cuenta con el CBU 987654321987654321");
+
+        actividad3.CuentaSueldo cuenta4 = new actividad3.CuentaSueldo(987654321987654321L);
         System.out.println(cuenta4);
+
+        System.out.println("\n----- Se intenta ingresar 10000");
 
         cuenta4.ingresar(10000);
 
+        System.out.println("\n----- Se intenta retirar 16000");
+
         cuenta4.retirar(16000);
+
+        System.out.println("\n----- Se intenta retirar -1");
 
         cuenta4.retirar(-1);
 
+        System.out.println("\n----- Se intenta retirar 3000");
+
         cuenta4.retirar(3000);
+
+        System.out.println("\n----- Se intenta ingresar 20000");
+
+        cuenta4.ingresar(20000);
+
+        System.out.println("\n----- Se intenta trasferir 16000 al CBU: 987654321123456789");
 
         cuenta4.trasferir(16000, 987654321123456789L);
 
+        System.out.println("\n----- Se intenta trasferir -1 al CBU: 987654321123456789");
+
         cuenta4.trasferir(-1, 987654321123456789L);
+
+        System.out.println("\n----- Se intenta trasferir 6000 al CBU: 987654321123456789");
 
         cuenta4.trasferir(6000, 987654321123456789L);
     }
